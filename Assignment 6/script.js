84463658816d049f3352347e45cbe82580a79d06
Weifeng Zhao <50115701+WeifengZhao1@users.noreply.github.com>
2022-03-03 15:59:46 -0500

    Assignment 6

---
+++ Assignment 6/script.js
@@ -0,0 +1,66 @@
+// Part A
+console.log('Part A')
+
+const tipCalculator = (val) => {
+    // var tip = 0.15;
+    if (val >= 30 && val <= 100) {
+        var tip = 0.15 * val;
+        var total = tip + val;
+        console.log('The bill value is ' + val, ' the tip is ' + tip + ' The final value would be ' + total)
+    }
+    else {
+        var tip = 0.20 * val;
+        var total = tip + val;
+        console.log('The bill value is ' + val, ' the tip is ' + tip + ' The final value would be ' + total)
+    }
+}
+
+
+tipCalculator(285)
+tipCalculator(26)
+tipCalculator(90)
+
+console.log(" ")
+
+
+// Part B
+console.log('Part B')
+
+const CovertCelsiusToFahrenheit = (val) => {
+    if (val >= -89.3 && val <= 56.7) {
+        const Fahrenheit = val * 1.8 + 32;
+        console.log(val + '°C is ' + Fahrenheit + '°F.')
+    }
+}
+
+const CovertFahrenheitToCelsius = (val) => {
+    if (val >= -128.74 && val <= 134.06) {
+        const Celsisu = (val - 32) * 5 / 9;
+        console.log(val + '°F is ' + Celsisu + '°C.')
+    }
+}
+
+
+CovertCelsiusToFahrenheit(100);
+CovertCelsiusToFahrenheit(0);
+CovertCelsiusToFahrenheit(10);
+
+
+console.log(' ');
+
+CovertFahrenheitToCelsius(32)
+CovertFahrenheitToCelsius(10)
+CovertFahrenheitToCelsius(102)
+
+
+// Part C
+const openModal = () => {
+    const modalWindow = document.getElementById("modal-window");
+    modalWindow.style.display = "flex";
+}
+
+
+const closeModal = () => {
+    const modalWindow = document.getElementById("modal-window");
+    modalWindow.style.display = "none";
+}
